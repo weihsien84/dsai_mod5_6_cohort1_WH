db99fbab92d9813db645cadbd52fc99bf1a346a8
weihsien84 <126282499+weihsien84@users.noreply.github.com>
2025-05-23 15:27:04 +0000

    1

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 from flask import Flask,request,render_template
 import google.generativeai as genai
 import os
+import sqlite3
+import datetime
 
 gemini_api_key = os.getenv("gemini_api_key")
 
@@ -11,9 +13,27 @@ model = genai.GenerativeModel("gemini-2.0-flash")
 
 app = Flask(__name__)
 
+first_time = 1
+
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    global first_time
+    if first_time == 1:
+        q = request.form.get("q")
+        print(q)
+        t = datetime.datetime.now()
+        conn = sqlite3.connect('user.db')
+        c = conn.cursor()
+        c.execute("insert into users(name,timestamp) values(?,?)",(q,t))
+        conn.commit()
+        c.close()
+        conn.close()
+        first_time=0
+    return(render_template("main.html"))
 
 @app.route("/gemini",methods=["GET","POST"])
 def gemini():
@@ -25,6 +45,40 @@ def gemini_reply():
     print(q)
     r = model.generate_content(q)
     return(render_template("gemini_reply.html",r=r.text))
+
+@app.route("/user_log",methods=["GET","POST"])
+def user_log():
+    # read
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("select * from users")
+    r=""
+    for row in c:
+        print(row)
+        r = r + str(row)
+    c.close()
+    conn.close()
+
+#    [Yee's code]  
+#    conn = sqlite3.connect('user.db')
+#    conn_row_factory = sqlite3.Row
+#    cursor = conn.cursor()
+#    cursor.execute('SELECT * FROM users order by timestamp')
+#    rows = cursor.fetchall
+#    conn.close()
+    return(render_template("user_log.html", r=r))
+
+
+@app.route("/delete_log",methods=["GET","POST"])
+def delete_log():
+# delete
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("delete from users")
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("delete_log.html"))
 
 if __name__ == "__main__":
     app.run()
