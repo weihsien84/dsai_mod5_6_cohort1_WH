e7224192265cf4723ab0a441bba3986ccddac5dc
weihsien84 <126282499+weihsien84@users.noreply.github.com>
2025-05-30 23:57:52 +0000

    1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #gemini
 
 from flask import Flask,request,render_template
-import google.generativeai as genai
+import google.generativeai as genai1
 from google import genai
 import os
 import sqlite3
@@ -14,8 +14,8 @@ gemini_api_key = os.getenv("gemini_api_key")
 gemini_client = genai.Client(api_key=gemini_api_key)
 gemini_model = "gemini_2.0-flash"
 
-genai.configure(api_key=gemini_api_key)
-model = genai.GenerativeModel("gemini-2.0-flash")
+genai1.configure(api_key=gemini_api_key)
+model = genai1.GenerativeModel("gemini-2.0-flash")
 
 gemini_telegram_token = os.getenv('GEMINI_TELEGRAM_TOKEN')
 
